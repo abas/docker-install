14f62be2049857e4994a12abbac94b2017718fc2
kankuu <akhmadbasir5@gmail.com>
2019-01-10 14:38:34 +0700

    update progressbar, on my own

--- sandbox/progressbar.sh
+++ sandbox/progressbar.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-pass='number1 number2 number 3 number4 number12 number13 number14 number15 number16'
-chk='number15'
 result="Not Found!"
 
 echo
 echo -n "Working... "
 echo -ne "\033[1;32m\033[7m\033[?25l"
 
-for i in $pass ; do
+complete=100
+for i in {1..100} ; do
    sleep .4s
-   if [ "$i" == "$chk" ]; then
-      result="  Found ^_^"
+   if [ "$i" == "$complete" ]; then
+      result="  Complete!"
       break
    else
       echo -n " "
