c5144b69b49ca7dfa5b760e99fa7b378d7da201f
kankuu <akhmadbasir5@gmail.com>
2019-01-11 23:27:47 +0700

    update testing is_number function

--- sandbox/test-fun.sh
+++ sandbox/test-fun.sh
@@ -2,4 +2,8 @@
 set -e
 source /Users/vander/.abas/docker-install/docker-install-helper.sh
 
-is_number "anone"
+if [[ $(is_number 9000) = "true" ]];then
+    echo "true"
+else
+    echo "false"
+fi
