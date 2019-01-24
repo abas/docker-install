fa86448c646bd1bdbe2f21f84f6cb7581a438a1d
abas <akhmadbasir5@gmail.com>
2019-01-24 07:53:36 +0700

    docker-compose-checker, still confuse ._. am i hungry?

--- sandbox/test-fun.sh
+++ sandbox/test-fun.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
-source /Users/vander/.abas/docker-install/docker-install-helper.sh
+source ../docker-install-helper.sh
 
+echo "|- testing is_number"
 if [[ $(is_number 9000) = "true" ]];then
     echo "true"
 else
     echo "false"
+fi
+
+echo ""
+echo "|- testing docker-compose-checker"
+
+if [[ docker-compose-checker == true ]]; then
+    echo "docker-compose exist at"
+    which docker-compose
+else
+    echo "docker-compose doesnt exist"
 fi
